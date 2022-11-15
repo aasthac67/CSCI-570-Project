422783f4a67ff60343e96da7195ec7f0f07ea739
Raviraj Shinde <61659345+Raviraj2000@users.noreply.github.com>
2022-11-15 02:04:52 -0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,40 @@
+import sys
+
+def read_input(f):
+    DNA = ['A', 'C', 'T', 'G']
+    words = []
+    with open(f) as file:
+        lines = file.read().splitlines()
+        word = ''
+        for line in lines:
+            if line[0] in DNA:
+                words.append(word)
+                word = line
+            else:
+                s = 0
+                s = int(line)
+                word = word[:s+1] + word + word[s+1:]
+        file.close()
+        
+    words.append(word)
+    return words
+
+
+def main():
+   words  = read_input(sys.argv[1])
+   print(words[1])
+   print('ACACTGACTACTGACTGGTGACTACTGACTGG')
+   print("=======================+=======================")
+   print(words[2])
+   print('TATTATACGCTATTATACGCGACGCGGACGCG')
+   return 
+
+main()
+
+
+            
+            
+            
+            
+            
+        
