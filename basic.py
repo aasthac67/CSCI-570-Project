8124ff076d4cd45763c2958245702acbd40ee968
Aastha Chowdhary <aasthac67@gmail.com>
2022-11-19 00:56:39 -0800

    Fixed basic.py

--- basic.py
+++ basic.py
@@ -3,24 +3,22 @@ from resource import *
 import time
 import psutil
 
-def sequence_alignment(x, y, alpha, DNA, delta):
+def sequence_alignment(x, y, alpha, delta):
     m = len(x)
     n = len(y)
 
     dp = [[0 for i in range(n+1)] for j in range(m+1)]
 
-    for i in range(m+1):
+    for i in range(1, m+1):
         dp[i][0] = i * delta
     
-    for i in range(n+1):
+    for i in range(1, n+1):
         dp[0][i] = i * delta
 
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if x[i-1] == y[j-1]:
-                dp[i][j] = dp[i-1][j-1]
-            else:
-                dp[i][j] = min(dp[i-1][j-1] + alpha[DNA[x[i-1]]][DNA[y[j-1]]],
+            string = x[i-1] + y [j-1]
+            dp[i][j] = min(dp[i-1][j-1] + alpha[string],
                                 dp[i-1][j] + delta,
                                 dp[i][j-1] + delta)
                 
@@ -45,8 +43,13 @@ def time_wrapper(s1, s2):
     start_time = time.time()
     
     #Call algorithms
-    alpha, DNA, delta = get_penalty()
-    dp = sequence_alignment(s1, s2, alpha, DNA, delta)
+    delta = 30
+    
+    alpha = {'AA': 0, 'CC': 0, 'GG': 0, 'TT': 0, 'AC': 110, 'CA': 110, 'AG': 48, 'GA': 48, 'AT': 94, 'TA': 94,
+             'CG': 118, 'GC': 118, 'CT': 48, 'TC': 48, 'GT': 110, 'TG': 110}
+
+    
+    dp = sequence_alignment(s1, s2, alpha, delta)
     form_sequence(dp)
     
     end_time = time.time()
@@ -54,10 +57,14 @@ def time_wrapper(s1, s2):
     return time_taken
 
 def main():
-    s1 = "ACACTGACTACTGACTGGTGACTACTGACTGG"
-    s2 = "TATTATACGCTATTATACGCGACGCGGACGCG"
+    s1 = "ACACACTGACTACTGACTGGTGACTACTGACTGGACTGACTACTGACTGGTGACTACTGACTGG"
+    s2 = "TATTATTATACGCTATTATACGCGACGCGGACGCGTATACGCTATTATACGCGACGCGGACGCG"
+    
+    time_wrapper(s1, s2)
 
+    '''
     print("The time taken by the algorithm is: " + time_wrapper(s1, s2))
     print("The process memory for the algorithm is: " + process_memory())
+    '''
 
 main()
